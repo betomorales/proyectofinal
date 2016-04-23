90ee6a96083a88d22bd262d3c5e2d7d511e7ae7a
betomorales <betomorales@outlook.com>
2016-04-22 23:09:16 -0600

    Proyecto Final

---
+++ Proyecto/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Listado de Productos de base de datos Northwind.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Pagina de contacto.";
+
+            return View();
+        }
+
+
+        public ActionResult Listado()
+        {
+            ViewBag.Message = "Listado de Productos de base de datos Northwind.";
+            return View();
+        }
+    }
+}
